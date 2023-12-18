d8a792916a4c283c8a6fe073a2311a4d857391ad
Subhadip757 <SubhadipGitHub@gmail.com>
2023-12-18 18:23:33 +0530

    update

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,48 +1,59 @@
-const firstPlusButton = document.getElementById('first-plus')
-const firstMinusButton = document.getElementById('first-minus');
+const plusButton = document.getElementById('plus1');
+const minusButton = document.getElementById('minus1');
+const hidden1 = document.getElementById('hidden1')
 
-const secondPlusButton = document.getElementById('second-plus')
-const secondMinusButton = document.getElementById('second-minus');
-
-const thirdPlusButton = document.getElementById('third-plus')
-const thirdMinusButton = document.getElementById('third-minus');
-
-const fourthPlusButton = document.getElementById('fourth-plus')
-const fourthMinusButton = document.getElementById('fourth-minus');
-
-const firstHidden = document.querySelector('.first-hidden')
-const secondHidden = document.querySelector('.second-hidden')
-const thirdHidden = document.querySelector('.third-hidden')
-const fourthHidden = document.querySelector('.fourth-hidden')
-
-firstPlusButton.addEventListener('click',()=>{
-    firstHidden.classList.remove('first-hidden')
+plusButton.addEventListener('click', () => {
+    hidden1.classList.remove("hidden1");
+    minusButton.style.display = 'block'
+    plusButton.style.display = 'none'
+})
+minusButton.addEventListener('click',()=>{
+    hidden1.classList.add('hidden1')
+    minusButton.style.display = 'none'
+    plusButton.style.display = 'block'
 })
 
-firstMinusButton.addEventListener('click',()=>{
-    firstHidden.classList.add('first-hidden');
+const plusButton2 = document.getElementById('plus2');
+const minusButton2 = document.getElementById('minus2');
+const hidden2 = document.getElementById('hidden2')
+
+plusButton2.addEventListener('click', () => {
+    hidden2.classList.remove("hidden2");
+    minusButton2.style.display = 'block'
+    plusButton2.style.display = 'none'
+})
+minusButton2.addEventListener('click',()=>{
+    hidden2.classList.add('hidden2')
+    minusButton2.style.display = 'none'
+    plusButton2.style.display = 'block'
 })
 
-secondPlusButton.addEventListener('click',()=>{
-    secondHidden.classList.remove('second-hidden')
+const plusButton3 = document.getElementById('plus3');
+const minusButton3 = document.getElementById('minus3');
+const hidden3 = document.getElementById('hidden3')
+
+plusButton3.addEventListener('click', () => {
+    hidden3.classList.remove("hidden3");
+    minusButton3.style.display = 'block'
+    plusButton3.style.display = 'none'
+})
+minusButton3.addEventListener('click',()=>{
+    hidden3.classList.add('hidden3')
+    minusButton3.style.display = 'none'
+    plusButton3.style.display = 'block'
 })
 
-secondMinusButton.addEventListener('click',()=>{
-    secondHidden.classList.add('second-hidden');
-})
+const plusButton4 = document.getElementById('plus4');
+const minusButton4 = document.getElementById('minus4');
+const hidden4 = document.getElementById('hidden4')
 
-thirdPlusButton.addEventListener('click',()=>{
-    thirdHidden.classList.remove('third-hidden')
+plusButton4.addEventListener('click', () => {
+    hidden4.classList.remove("hidden4");
+    minusButton4.style.display = 'block'
+    plusButton4.style.display = 'none'
 })
-
-thirdMinusButton.addEventListener('click',()=>{
-    thirdHidden.classList.add('third-hidden');
-})
-
-fourthPlusButton.addEventListener('click',()=>{
-    fourthHidden.classList.remove('fourth-hidden')
-})
-
-fourthMinusButton.addEventListener('click',()=>{
-    fourthHidden.classList.add('fourth-hidden');
+minusButton4.addEventListener('click',()=>{
+    hidden4.classList.add('hidden4')
+    minusButton4.style.display = 'none'
+    plusButton4.style.display = 'block'
 })
